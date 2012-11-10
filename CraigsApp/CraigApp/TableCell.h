44f6bf21a4ec84fffd510e7f2df249e562de1c47
Hayley Wu <hwu@Hayleys-MacBook-Pro.local>
2012-11-09 19:34:45 -0800

    added pngs and check/uncheck buttons, implemented these in listing view.

--- CraigsApp/CraigApp/TableCell.h
+++ CraigsApp/CraigApp/TableCell.h
@@ -8,11 +8,21 @@
 
 @interface TableCell : UITableViewCell
 
+
 @property (nonatomic, weak) IBOutlet UILabel *titleLabel;
 @property (nonatomic, weak) IBOutlet UILabel *locationLabel;
 @property (nonatomic, weak) IBOutlet UILabel *priceLabel;
 @property (nonatomic, weak) IBOutlet UILabel *bedLabel;
 @property (nonatomic, weak) IBOutlet UILabel *bathLabel;
 @property (nonatomic, weak) IBOutlet UILabel *dateLabel;
+@property (nonatomic, weak) IBOutlet UIButton *addToFavButton;
+@property (nonatomic, weak ) IBOutlet UIButton *addToMapButton;
+
+@property (nonatomic) BOOL favChecked;
+@property (nonatomic) BOOL mapChecked;
+
+
+- (IBAction) checkFav: (id) sender;
+- (IBAction) checkMap: (id) sender;
 
 @end
--- CraigsApp/CraigApp/TableCell.m
+++ CraigsApp/CraigApp/TableCell.m
@@ -10,6 +10,11 @@
 
 @synthesize titleLabel, locationLabel, priceLabel, bedLabel, bathLabel, dateLabel;
 
+@synthesize favChecked, mapChecked;
+
+@synthesize addToFavButton, addToMapButton;
+
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
@@ -19,11 +24,32 @@
     return self;
 }
 
+
+
 - (void)setSelected:(BOOL)selected animated:(BOOL)animated
 {
     [super setSelected:selected animated:animated];
 
     // Configure the view for the selected state
 }
+
+
+
+- (IBAction) checkFav:(id)sender
+{
+	self.favChecked= !self.favChecked; // toggles
+	UIImage *favCheckedImage = (self.favChecked) ? [UIImage imageNamed:@"checked.png"] : [UIImage imageNamed:@"unchecked.png"];
+	[addToFavButton setImage:favCheckedImage forState:UIControlStateNormal];
+}
+
+
+- (IBAction) checkMap: (id) sender;
+{
+    self.mapChecked = !self.mapChecked; // toggles
+	UIImage *mapCheckedImage = (self.mapChecked) ? [UIImage imageNamed:@"checked.png"] : [UIImage imageNamed:@"unchecked.png"];
+	[addToMapButton setImage:mapCheckedImage forState:UIControlStateNormal];
+}
+
+
 
 @end
