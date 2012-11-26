3af57d4f9e8ebd3f28e0e68e1d5e05c7472dbd04
theElephant <hwu@Hayleys-MacBook-Pro.local>
2012-11-25 19:10:03 -0800

    placeholder xml parser not connected to rest of app

---
+++ CraigsApp/CraigApp/ParsedListings.h
@@ -0,0 +1,13 @@
+//
+//  ParsedListings.h
+//  ISA
+//
+//  Created by Hayley Wu on 11/25/12.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ParsedListings : NSObject
+
+@end
---
+++ CraigsApp/CraigApp/ParsedListings.m
@@ -0,0 +1,13 @@
+//
+//  ParsedListings.m
+//  ISA
+//
+//  Created by Hayley Wu on 11/25/12.
+//
+//
+
+#import "ParsedListings.h"
+
+@implementation ParsedListings
+
+@end
