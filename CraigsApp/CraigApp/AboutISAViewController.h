4aed17d113255f02f5a778c9494afc1ab77a7131
theElephant <hwu@dhcp138.home>
2012-12-03 23:10:21 -0800

    added vc for about isa as well as icon

---
+++ CraigsApp/CraigApp/AboutISAViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutISAViewController.h
+//  ISA
+//
+//  Created by Hayley Wu on 12/3/12.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutISAViewController : UIViewController
+
+@end
---
+++ CraigsApp/CraigApp/AboutISAViewController.m
@@ -0,0 +1,38 @@
+//
+//  AboutISAViewController.m
+//  ISA
+//
+//  Created by Hayley Wu on 12/3/12.
+//
+//
+
+#import "AboutISAViewController.h"
+
+@interface AboutISAViewController ()
+
+@end
+
+@implementation AboutISAViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
