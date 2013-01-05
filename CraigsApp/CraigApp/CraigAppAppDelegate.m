0b6ab6e17318c9fba64d14fda08eb1f2ed8ece0c
theElephant <hwu@dhcp138.home>
2013-01-05 11:43:56 -0800

    call saveLastTab upon entering background

--- CraigsApp/CraigApp/CraigAppAppDelegate.m
+++ CraigsApp/CraigApp/CraigAppAppDelegate.m
@@ -52,6 +52,7 @@ NSString *startTab = @"startTab";
     Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later. 
     If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
     */
+    [self saveLastTab];
 }
 
 - (void)applicationWillEnterForeground:(UIApplication *)application
